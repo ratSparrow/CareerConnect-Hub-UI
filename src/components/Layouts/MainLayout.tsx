c346b22e402ac7c3d6949b9ba75fe073eb91306d
M Rafiul Alam Tonmoy <mrafiul.alam7@gmail.com>
2024-01-09 23:32:00 +0600

    added footer

--- src/components/Layouts/MainLayout.tsx
+++ src/components/Layouts/MainLayout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../shared/Navbar";
+import Footer from "../shared/Footer";
 
 const MainLayout = () => {
   return (
     <>
       <NavBar />
       <Outlet />
+      <Footer />
     </>
   );
 };
