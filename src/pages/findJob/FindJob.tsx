00306cad2c51881fd3ef965a63ba56ab90257435
shapna28 <shapna088@gmail.com>
2024-01-11 03:19:46 +0600

    Done search functionality

--- src/pages/findJob/FindJob.tsx
+++ src/pages/findJob/FindJob.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import Search from "antd/es/input/Search";
+
 import { useJobsQuery } from "../../redux/api/jobApi";
-import { Button, Card, Col, Flex, Row } from "antd";
+import { Button, Card, Col, Flex, Input, Row } from "antd";
 import { IJobData } from "../../types";
 import { RiseOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
@@ -14,23 +14,22 @@ const FindJob = () => {
   const query: Record<string, any> = {};
   const { data } = useJobsQuery({ ...query });
   const jobData = data?.data?.data;
-  const [searchJob, setSearchJob] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
 
   // Search jobs
   const handleSearch = async () => {
-    console.log(data);
     try {
       const res = await fetch(
-        `https://career-connect-hub-api.vercel.app/api/v1/jobs?searchTerm=${searchJob}`
+        `https://career-connect-hub-api.vercel.app/api/v1/jobs?searchTerm=${searchQuery}`
       );
       const data = await res.json();
-      console.log(data);
-      setSearchJob(data?.data?.data);
+      setSearchResults(data?.data?.data);
     } catch (error) {
       console.error("Error:", error);
     }
   };
-  console.log(searchJob);
+
   return (
     <div style={{ minHeight: "100vh", margin: "30px 50px" }}>
       <div
@@ -82,77 +81,147 @@ const FindJob = () => {
               preferences
             </span>
           </p>
-          <Search
-            placeholder="Search for jobs"
-            enterButton
-            size="large"
-            onSearch={handleSearch}
-          />
+          <Flex>
+            <Input
+              style={{}}
+              placeholder="Search for jobs"
+              size="large"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <Button type="primary" onClick={handleSearch} size="large">
+              Search
+            </Button>
+          </Flex>
         </Col>
 
         <Col xs={24} sm={24} md={16} lg={16} xl={16}>
-          {jobData?.map((job: IJobData) => (
-            <div
-              key={job?._id}
-              style={{
-                boxShadow: "0px 0px 10px 0px rgba(0,0,0,0.1)",
-                borderRadius: "5px",
-                marginBottom: "10px",
-              }}
-            >
-              <div
-                style={{
-                  padding: "0 20px",
-                  paddingTop: "20px",
-                  color: "blue",
-                  display: "flex",
-                  gap: "5px",
-                }}
-              >
-                <RiseOutlined /> <p>Active Hiring</p>
-              </div>
+          {searchResults.length > 0 // Render search results if available
+            ? searchResults.map((job: IJobData) => (
+                <div
+                  key={job?._id}
+                  style={{
+                    boxShadow: "0px 0px 10px 0px rgba(0,0,0,0.1)",
+                    borderRadius: "5px",
+                    marginBottom: "10px",
+                  }}
+                >
+                  <div
+                    style={{
+                      padding: "0 20px",
+                      paddingTop: "20px",
+                      color: "blue",
+                      display: "flex",
+                      gap: "5px",
+                    }}
+                  >
+                    <RiseOutlined /> <p>Active Hiring</p>
+                  </div>
 
-              <Card bordered={false}>
-                <h3 style={{ fontSize: "20px", marginBottom: "10px" }}>
-                  {job?.title}
-                </h3>
-                <p>{job?.company}</p>
-                <br />
-                <div>
-                  <h4>Location</h4>
-                  <p>{job?.location}</p>
+                  <Card bordered={false}>
+                    <h3 style={{ fontSize: "20px", marginBottom: "10px" }}>
+                      {job?.title}
+                    </h3>
+                    <p>{job?.company}</p>
+                    <br />
+                    <div>
+                      <h4>Location</h4>
+                      <p>{job?.location}</p>
+                    </div>
+
+                    <br />
+
+                    <Flex wrap="wrap" gap="20px">
+                      <div>
+                        <h4>Job Type</h4>
+                        <p>{job?.jobType}</p>
+                      </div>
+                      <div>
+                        <h4>Joining Date</h4>
+                        <p>{job?.joiningDate}</p>
+                      </div>
+                      <div>
+                        <h4>CTC</h4>
+                        <p>{job?.salary}</p>
+                      </div>
+                      <div>
+                        <h4>Experience</h4>
+                        <p>{job?.experienceLevel}</p>
+                      </div>
+                    </Flex>
+                    <br />
+                    <Flex wrap="wrap" gap="small" justify="end" align="center">
+                      <Link to={`/details/${job?._id}`}>View Details</Link>
+                      <Button type="primary" onClick={() => setOpen(true)}>
+                        Apply Now
+                      </Button>
+                    </Flex>
+                  </Card>
                 </div>
+              ))
+            : // Otherwise, render caregivers data
+              jobData?.map((job: IJobData) => (
+                <div
+                  key={job?._id}
+                  style={{
+                    boxShadow: "0px 0px 10px 0px rgba(0,0,0,0.1)",
+                    borderRadius: "5px",
+                    marginBottom: "10px",
+                  }}
+                >
+                  <div
+                    style={{
+                      padding: "0 20px",
+                      paddingTop: "20px",
+                      color: "blue",
+                      display: "flex",
+                      gap: "5px",
+                    }}
+                  >
+                    <RiseOutlined /> <p>Active Hiring</p>
+                  </div>
 
-                <br />
+                  <Card bordered={false}>
+                    <h3 style={{ fontSize: "20px", marginBottom: "10px" }}>
+                      {job?.title}
+                    </h3>
+                    <p>{job?.company}</p>
+                    <br />
+                    <div>
+                      <h4>Location</h4>
+                      <p>{job?.location}</p>
+                    </div>
 
-                <Flex wrap="wrap" gap="20px">
-                  <div>
-                    <h4>Job Type</h4>
-                    <p>{job?.jobType}</p>
-                  </div>
-                  <div>
-                    <h4>Joining Date</h4>
-                    <p>{job?.joiningDate}</p>
-                  </div>
-                  <div>
-                    <h4>CTC</h4>
-                    <p>{job?.salary}</p>
-                  </div>
-                  <div>
-                    <h4>Experience</h4>
-                    <p>{job?.experienceLevel}</p>
-                  </div>
-                </Flex>
-                <br />
-                <Flex wrap="wrap" gap="small" justify="end" align="center">
-                  <Link to={`/details/${job?._id}`}>View Details</Link>
-                  <Button type="primary" onClick={() => setOpen(true)}>
-                    Apply Now
-                  </Button>
-                </Flex>
-              </Card>
-            </div>
-          ))}
+                    <br />
+
+                    <Flex wrap="wrap" gap="20px">
+                      <div>
+                        <h4>Job Type</h4>
+                        <p>{job?.jobType}</p>
+                      </div>
+                      <div>
+                        <h4>Joining Date</h4>
+                        <p>{job?.joiningDate}</p>
+                      </div>
+                      <div>
+                        <h4>CTC</h4>
+                        <p>{job?.salary}</p>
+                      </div>
+                      <div>
+                        <h4>Experience</h4>
+                        <p>{job?.experienceLevel}</p>
+                      </div>
+                    </Flex>
+                    <br />
+                    <Flex wrap="wrap" gap="small" justify="end" align="center">
+                      <Link to={`/details/${job?._id}`}>View Details</Link>
+                      <Button type="primary" onClick={() => setOpen(true)}>
+                        Apply Now
+                      </Button>
+                    </Flex>
+                  </Card>
+                </div>
+              ))}
         </Col>
       </Row>
       <GlobalModal open={open} setOpen={setOpen} width={650} title={""}>
